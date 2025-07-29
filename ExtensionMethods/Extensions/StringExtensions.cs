323f0cf47407553e44acd0f9884bf4f9b1646631
Fabricio Dias <fabricio270909@gmail.com>
2025-07-28 22:20:06 -0300

    feat: Example String Extension method

---
+++ ExtensionMethods/Extensions/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace System;
+
+static class StringExtensions {
+    public static string Cut(this String thisObj, int length) {
+        if (thisObj.Length <= length) {
+            return thisObj;
+        }
+        
+        return thisObj.Substring(0, length) + "...";
+    }
+}
--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -2,7 +2,10 @@
 
 class Program {
     static void Main(string[] args) {
-        DateTime dt = new DateTime(2025, 07, 28, 20, 10, 45);
+        DateTime dt = new DateTime(2025, 06, 28, 20, 10, 45);
         Console.WriteLine(dt.ElapsedTime());
+        
+        string s1 = "This is a very long string that needs to be cut off at some point.";
+        Console.WriteLine(s1.Cut(15));
     }
 }
